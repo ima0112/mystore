3c65572722c1d39458a7ff41121ba342a298d5a9
ima0112 <imandracardenas0@gmail.com>
2024-07-09 19:51:24 -0400

    Login form

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:mystore/common/styles/spacing_styles.dart';
 import 'package:mystore/utils/constants/image_strings.dart';
 import 'package:mystore/utils/constants/sizes.dart';
@@ -18,7 +19,7 @@ class LoginScreen extends StatelessWidget {
           padding: MySpacingStyles.paddingWithAppBarHeight,
           child: Column(
             children: [
-              // Header: Logo, Title & Subtitle
+              /// Header: Logo, Title & Subtitle
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -40,6 +41,80 @@ class LoginScreen extends StatelessWidget {
                     style: Theme.of(context).textTheme.bodyMedium,
                   ),
                 ],
+              ),
+
+              /// Form
+              Form(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: MySizes.spaceBtwSections,
+                  ),
+                  child: Column(
+                    children: [
+                      /// Email
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.direct_right),
+                          labelText: MyTexts.email,
+                        ),
+                      ),
+                      const SizedBox(height: MySizes.spaceBtwInputFields),
+
+                      /// Password
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.password_check),
+                          labelText: MyTexts.password,
+                          suffixIcon: Icon(Iconsax.eye_slash),
+                        ),
+                      ),
+                      const SizedBox(height: MySizes.spaceBtwInputFields / 2),
+
+                      /// Remember Me & Forget Password
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          /// Remember Me
+                          Row(
+                            children: [
+                              Checkbox(value: true, onChanged: (value) {}),
+                              const Text(MyTexts.rememberMe),
+                            ],
+                          ),
+
+                          ///Forget Password
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              MyTexts.forgetPassword,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: MySizes.spaceBtwSections),
+
+                      /// Sign In Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(MyTexts.signIn),
+                        ),
+                      ),
+                      const SizedBox(height: MySizes.spaceBtwItems),
+
+                      /// Create Account Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: const Text(MyTexts.createAccount),
+                        ),
+                      ),
+                      const SizedBox(height: MySizes.spaceBtwSections),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
