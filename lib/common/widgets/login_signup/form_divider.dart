a93d2472087ff73621146c7423cd6d91023e62bd
ima0112 <imandracardenas0@gmail.com>
2024-07-14 13:50:40 -0400

    Code refactoring

---
+++ lib/common/widgets/login_signup/form_divider.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/text_strings.dart';
+import 'package:mystore/utils/extensions/extensions.dart';
+import 'package:mystore/utils/helpers/helper_functions.dart';
+
+class FormDivider extends StatelessWidget {
+  const FormDivider({
+    super.key,
+    required this.dividerText,
+  });
+
+  final String dividerText;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MyHelperFunctions.isDarkMode(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: Divider(
+            color: dark ? MyColors.darkGrey : MyColors.grey,
+            indent: 60,
+            endIndent: 5,
+            thickness: 0.5,
+          ),
+        ),
+        Text(
+          dividerText,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+        Flexible(
+          child: Divider(
+            color: dark ? MyColors.darkGrey : MyColors.grey,
+            indent: 5,
+            endIndent: 60,
+            thickness: 0.5,
+          ),
+        ),
+      ],
+    );
+  }
+}
