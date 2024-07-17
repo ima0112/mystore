50f20c079369888d487aec9c1045772af9c916db
ima0112 <imandracardenas0@gmail.com>
2024-07-16 23:28:04 -0400

    Curved Edges

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:mystore/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:mystore/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:mystore/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,26 +13,31 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              color: MyColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: -150,
-                      right: -250,
-                      child: MyCircularContainer(
-                          backgroundColor: MyColors.textWhite.withOpacity(0.1)),
-                    ),
-                    Positioned(
-                      top: 100,
-                      right: -300,
-                      child: MyCircularContainer(
-                          backgroundColor: MyColors.textWhite.withOpacity(0.1)),
-                    ),
-                  ],
+            ClipPath(
+              clipper: MyCustomCurvedEdges(),
+              child: Container(
+                color: MyColors.primary,
+                padding: const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: -150,
+                        right: -250,
+                        child: MyCircularContainer(
+                            backgroundColor:
+                                MyColors.textWhite.withOpacity(0.1)),
+                      ),
+                      Positioned(
+                        top: 100,
+                        right: -300,
+                        child: MyCircularContainer(
+                            backgroundColor:
+                                MyColors.textWhite.withOpacity(0.1)),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
