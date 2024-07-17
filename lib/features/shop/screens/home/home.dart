85095e62dca902f309a0bcf67ef401502da48820
ima0112 <imandracardenas0@gmail.com>
2024-07-16 23:52:55 -0400

    Custom App Bar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mystore/common/widgets/appbar/appbar.dart';
 
 import 'package:mystore/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,7 +15,31 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             MyPrimaryHeaderContainer(
-              child: Container(),
+              child: Column(
+                children: [
+                  MyAppBar(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          MyTexts.homeAppbarTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium!
+                              .apply(color: MyColors.grey),
+                        ),
+                        Text(
+                          MyTexts.homeAppbarSubTitle,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .apply(color: MyColors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
