158c6c50e1b6526987456d0e044d4271effbadf5
ima0112 <imandracardenas0@gmail.com>
2024-07-16 23:36:37 -0400

    Code refactoring

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:mystore/common/widgets/custom_shapes/containers/circular_container.dart';
-import 'package:mystore/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,33 +11,8 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ClipPath(
-              clipper: MyCustomCurvedEdges(),
-              child: Container(
-                color: MyColors.primary,
-                padding: const EdgeInsets.all(0),
-                child: SizedBox(
-                  height: 400,
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: -150,
-                        right: -250,
-                        child: MyCircularContainer(
-                            backgroundColor:
-                                MyColors.textWhite.withOpacity(0.1)),
-                      ),
-                      Positioned(
-                        top: 100,
-                        right: -300,
-                        child: MyCircularContainer(
-                            backgroundColor:
-                                MyColors.textWhite.withOpacity(0.1)),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            MyPrimaryHeaderContainer(
+              child: Container(),
             ),
           ],
         ),
