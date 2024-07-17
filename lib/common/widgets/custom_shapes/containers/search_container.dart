6322c51d018d06be03fbf9113355a396b492c2d3
ima0112 <imandracardenas0@gmail.com>
2024-07-17 14:57:06 -0400

    Search Bar

---
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:iconsax/iconsax.dart';
+
+import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/sizes.dart';
+import 'package:mystore/utils/device/device_utility.dart';
+import 'package:mystore/utils/helpers/helper_functions.dart';
+
+class MySearchContainer extends StatelessWidget {
+  const MySearchContainer({
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+    this.onTap,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = MyHelperFunctions.isDarkMode(context);
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: MySizes.defaultSpace),
+        child: Container(
+          width: MyDeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(MySizes.md),
+          decoration: BoxDecoration(
+            color: showBackground
+                ? dark
+                    ? MyColors.dark
+                    : MyColors.light
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(MySizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: MyColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: MyColors.darkGrey),
+              const SizedBox(width: MySizes.spaceBtwItems),
+              Text(text, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
