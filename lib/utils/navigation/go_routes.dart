a71b83cb9cb5529df7a2476a1c5bd97c341af837
ima0112 <imandracardenas0@gmail.com>
2024-07-16 22:55:25 -0400

    Bottom Navigation Bar

--- lib/utils/navigation/go_routes.dart
+++ lib/utils/navigation/go_routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'package:go_router/go_router.dart';
-import 'package:mystore/common/widgets/success_screen/success_screen.dart';
+
 import 'package:mystore/features/authentication/screens/login/login.dart';
 import 'package:mystore/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:mystore/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:mystore/features/authentication/screens/password_configuration/reset_password.dart';
 import 'package:mystore/features/authentication/screens/signup/signup.dart';
 import 'package:mystore/features/authentication/screens/signup/verify_email.dart';
+import 'package:mystore/navigation_menu.dart';
 
 enum MyRoutes {
   onboarding,
@@ -41,11 +43,14 @@ class AppRoute {
     navigatorKey: _rootNavigatorKey,
     initialLocation: _onboarding,
     routes: [
+      // Onboarding
       GoRoute(
         path: _onboarding,
         name: MyRoutes.onboarding.name,
         builder: (context, state) => const OnboardingScreen(),
       ),
+
+      // Login
       GoRoute(
         path: _login,
         name: MyRoutes.login.name,
@@ -73,6 +78,59 @@ class AppRoute {
             path: _resetPassword,
             name: MyRoutes.resetPassword.name,
             builder: (context, state) => const ResetPasswordScreen(),
+          ),
+        ],
+      ),
+
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) {
+          return NavigationMenu(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: '/home',
+            name: 'home',
+            pageBuilder: (BuildContext context, GoRouterState state) =>
+                NoTransitionPage<void>(
+              key: state.pageKey,
+              child: const Center(
+                child: Text('Home'),
+              ),
+            ),
+          ),
+          GoRoute(
+            path: '/store',
+            name: 'store',
+            pageBuilder: (BuildContext context, GoRouterState state) =>
+                NoTransitionPage<void>(
+              key: state.pageKey,
+              child: const Center(
+                child: Text('Store'),
+              ),
+            ),
+          ),
+          GoRoute(
+            path: '/wishlist',
+            name: 'wishlist',
+            pageBuilder: (BuildContext context, GoRouterState state) =>
+                NoTransitionPage<void>(
+              key: state.pageKey,
+              child: const Center(
+                child: Text('Wishlist'),
+              ),
+            ),
+          ),
+          GoRoute(
+            path: '/profile',
+            name: 'profile',
+            pageBuilder: (BuildContext context, GoRouterState state) =>
+                NoTransitionPage<void>(
+              key: state.pageKey,
+              child: const Center(
+                child: Text('Profile'),
+              ),
+            ),
           ),
         ],
       ),
