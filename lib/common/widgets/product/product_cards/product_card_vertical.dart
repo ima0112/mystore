cc9032cd2a17b036b41d7f5dc6c7c490ade807c2
ima0112 <imandracardenas0@gmail.com>
2024-07-18 22:43:32 -0400

    Vertical Card Thumbnail, Wishlist Button, Discount Tag

--- lib/common/widgets/product/product_cards/product_card_vertical.dart
+++ lib/common/widgets/product/product_cards/product_card_vertical.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 import 'package:mystore/common/styles/shadows.dart';
 import 'package:mystore/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:mystore/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:mystore/common/widgets/icons/circular_icon.dart';
+import 'package:mystore/common/widgets/images/rounded_image.dart';
 import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/image_strings.dart';
 import 'package:mystore/utils/constants/sizes.dart';
 import 'package:mystore/utils/helpers/helper_functions.dart';
 
@@ -27,8 +31,49 @@ class MyProductCardVertical extends StatelessWidget {
           /// Thumbnail, Wishlist Button, Discount Tag
           MyRoundedContainer(
             height: 180,
-            padding: const EdgeInsets.all(MySizes.md),
             backgroundColor: dark ? MyColors.black : MyColors.light,
+            child: Stack(
+              children: [
+                /// Thumbnail Image
+                /// MyRoundedImage(
+                const MyRoundedImage(
+                  imageUrl: MyImages.productImage2,
+                  applyImageRadius: true,
+                ),
+
+                /// Sale Tag
+                Positioned(
+                  top: 16,
+                  left: 4,
+                  child: MyRoundedContainer(
+                    radius: MySizes.sm,
+                    backgroundColor: MyColors.secondary.withOpacity(0.8),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: MySizes.sm,
+                      vertical: MySizes.xs,
+                    ),
+                    child: Text(
+                      '25%',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge!
+                          .apply(color: MyColors.black),
+                    ),
+                  ),
+                ),
+
+                /// Favorite Icon Button
+                Positioned(
+                  top: 4,
+                  right: 4,
+                  child: MyCircularIcon(
+                    icon: Iconsax.heart5,
+                    color: Colors.red,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
