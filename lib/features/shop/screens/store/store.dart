41fd0c15738d33fc02a53ba1bf20a00ab1dab721
ima0112 <imandracardenas0@gmail.com>
2024-07-20 13:00:57 -0400

    Store Featured Brands Cards

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mystore/common/widgets/appbar/appbar.dart';
+import 'package:mystore/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:mystore/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:mystore/common/widgets/images/circular_image.dart';
+import 'package:mystore/common/widgets/layouts/grid_layout.dart';
 import 'package:mystore/common/widgets/product/cart/cart_menu_icon.dart';
+import 'package:mystore/common/widgets/texts/brand_title_text_with_verified_icon.dart';
 import 'package:mystore/common/widgets/texts/section_heading.dart';
 import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/enums.dart';
+import 'package:mystore/utils/constants/image_strings.dart';
 import 'package:mystore/utils/constants/sizes.dart';
 import 'package:mystore/utils/helpers/helper_functions.dart';
 
@@ -56,7 +62,61 @@ class StoreScreen extends StatelessWidget {
                       onPressed: () {},
                     ),
                     const SizedBox(height: MySizes.spaceBtwItems / 1.5),
-                    
+
+                    MyGridLayout(
+                      itemCount: 4,
+                      mainAxisExtent: 80,
+                      itemBuilder: (_, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: MyRoundedContainer(
+                            padding: const EdgeInsets.all(MySizes.sm),
+                            showBorder: true,
+                            backgroundColor: Colors.transparent,
+                            child: Row(
+                              children: [
+                                /// Icon
+                                Flexible(
+                                  child: MyCircularImage(
+                                    isNetworkImage: false,
+                                    image: MyImages.clothIcon,
+                                    backgroundColor: Colors.transparent,
+                                    overlayColor:
+                                        MyHelperFunctions.isDarkMode(context)
+                                            ? MyColors.white
+                                            : MyColors.black,
+                                  ),
+                                ),
+                                const SizedBox(
+                                    height: MySizes.spaceBtwItems / 2),
+
+                                /// Text
+                                Expanded(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const MyBrandTitleWithVerifiedIcon(
+                                        title: 'Close up',
+                                        brandTextSize: TextSizes.large,
+                                      ),
+                                      Text(
+                                        '256 products',
+                                        overflow: TextOverflow.ellipsis,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelMedium,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ],
                 ),
               ),
