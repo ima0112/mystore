d232ff50afef3ff5e9d8faf71a20abd915ecc7a1
ima0112 <imandracardenas0@gmail.com>
2024-07-20 12:25:39 -0400

    Store Screen Search Bar

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -15,12 +15,14 @@ class MySearchContainer extends StatelessWidget {
     this.showBackground = true,
     this.showBorder = true,
     this.onTap,
+    this.padding = const EdgeInsets.symmetric(horizontal: MySizes.defaultSpace),
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
   final VoidCallback? onTap;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,7 @@ class MySearchContainer extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: MySizes.defaultSpace),
+        padding: padding,
         child: Container(
           width: MyDeviceUtils.getScreenWidth(context),
           padding: const EdgeInsets.all(MySizes.md),
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mystore/common/widgets/appbar/appbar.dart';
+import 'package:mystore/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:mystore/common/widgets/product/cart/cart_menu_icon.dart';
+import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/sizes.dart';
+import 'package:mystore/utils/helpers/helper_functions.dart';
 
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
@@ -16,6 +20,40 @@ class StoreScreen extends StatelessWidget {
         actions: [
           MyCartCounterIcon(onPressed: () {}),
         ],
+      ),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              pinned: true,
+              floating: true,
+              backgroundColor: MyHelperFunctions.isDarkMode(context)
+                  ? MyColors.black
+                  : MyColors.white,
+              expandedHeight: 440,
+              flexibleSpace: Padding(
+                padding: const EdgeInsets.all(MySizes.defaultSpace),
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: const [
+                    /// Search Bar
+                    SizedBox(height: MySizes.spaceBtwItems),
+                    MySearchContainer(
+                      text: 'Search in Store',
+                      showBorder: true,
+                      showBackground: false,
+                      padding: EdgeInsets.zero,
+                    ),
+                    SizedBox(height: MySizes.spaceBtwSections),
+                  ],
+                ),
+              ),
+            ),
+          ];
+        },
+        body: Container(),
       ),
     );
   }
