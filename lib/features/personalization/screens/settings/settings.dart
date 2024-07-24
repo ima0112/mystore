f75180e733b0b289f4bf2d93f6b632257246c068
ima0112 <imandracardenas0@gmail.com>
2024-07-24 18:09:40 -0400

    Account Settings

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -3,7 +3,9 @@ import 'package:iconsax/iconsax.dart';
 import 'package:mystore/common/widgets/appbar/appbar.dart';
 import 'package:mystore/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:mystore/common/widgets/images/circular_image.dart';
+import 'package:mystore/common/widgets/list_tiles/settings_menu_tile.dart';
 import 'package:mystore/common/widgets/list_tiles/user_profile_tile.dart';
+import 'package:mystore/common/widgets/texts/section_heading.dart';
 import 'package:mystore/utils/constants/colors.dart';
 import 'package:mystore/utils/constants/image_strings.dart';
 import 'package:mystore/utils/constants/sizes.dart';
@@ -33,6 +35,64 @@ class SettingsScreen extends StatelessWidget {
 
                   /// User Profile Card
                   const MyUserProfileTile(),
+                ],
+              ),
+            ),
+
+            /// Body
+            Padding(
+              padding: const EdgeInsets.all(MySizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// Account Settings
+                  const MySectionHeading(
+                    title: 'Account Settings',
+                    showActionButton: false,
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwItems),
+
+                  MySettingMenuTile(
+                    icon: Iconsax.safe_home,
+                    title: 'My Addresses',
+                    subtitle: 'Set shopping delivery address',
+                    onTap: () {},
+                  ),
+                  MySettingMenuTile(
+                    icon: Iconsax.shopping_cart,
+                    title: 'My Cart',
+                    subtitle: 'Add, remove products and move to checkout',
+                    onTap: () {},
+                  ),
+                  MySettingMenuTile(
+                    icon: Iconsax.bag_tick,
+                    title: 'My Orders',
+                    subtitle: 'In-progress and Completed Orders',
+                    onTap: () {},
+                  ),
+                  MySettingMenuTile(
+                    icon: Iconsax.bank,
+                    title: 'Bank Account',
+                    subtitle: 'Withdraw balance to registered bank account',
+                    onTap: () {},
+                  ),
+                  MySettingMenuTile(
+                    icon: Iconsax.discount_shape,
+                    title: 'My Coupons',
+                    subtitle: 'List of all the discounted coupons',
+                    onTap: () {},
+                  ),
+                  MySettingMenuTile(
+                    icon: Iconsax.notification,
+                    title: 'Notifications',
+                    subtitle: 'Set any kind of notification message',
+                    onTap: () {},
+                  ),
+                  MySettingMenuTile(
+                    icon: Iconsax.security_card,
+                    title: 'Account Privacy',
+                    subtitle: 'Manage data usage and connected accounts',
+                    onTap: () {},
+                  ),
                 ],
               ),
             ),
