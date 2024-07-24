c53163d3fb2ba87bef21b2de40625ab7efde206f
ima0112 <imandracardenas0@gmail.com>
2024-07-24 18:34:06 -0400

    Proile Picture

--- lib/features/personalization/screens/profile/profile.dart
+++ lib/features/personalization/screens/profile/profile.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:mystore/common/widgets/appbar/appbar.dart';
+import 'package:mystore/common/widgets/images/circular_image.dart';
+import 'package:mystore/utils/constants/image_strings.dart';
+import 'package:mystore/utils/constants/sizes.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: MyAppBar(
         showBackArrow: true,
         title: Text('Profile'),
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(MySizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Profile Picture
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const MyCircularImage(
+                        image: MyImages.user, width: 80, height: 80),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Change Profile Picture'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
