a737bdd05b26be1d8da77aad8423f545784067e8
ima0112 <imandracardenas0@gmail.com>
2024-07-24 17:27:27 -0400

    Remove height from Primary Header Container

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -50,7 +50,6 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                   ),
-                  SizedBox(height: MySizes.spaceBtwSections),
                 ],
               ),
             ),
