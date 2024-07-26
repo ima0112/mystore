1800064f4662a9ef6914aca1d9c5102fe073d41b
ima0112 <imandracardenas0@gmail.com>
2024-07-25 23:37:47 -0400

    Rating and Share Widget

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 
 import 'package:mystore/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:mystore/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:mystore/utils/constants/sizes.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   const ProductDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        child: const Column(
+        child: Column(
           children: [
             /// Product Image Slider
-            ProductImageSlider(),
+            const ProductImageSlider(),
+
+            /// Product Details
+            Padding(
+              padding: const EdgeInsets.only(
+                  right: MySizes.defaultSpace,
+                  left: MySizes.defaultSpace,
+                  bottom: MySizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// Rating & Share Button
+                  RatingAndShare(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
