92543e98b74d598d134c3b622cbe2b7e761582a2
ima0112 <imandracardenas0@gmail.com>
2024-07-25 23:54:40 -0400

    Product Title, Stock Status and Brand

--- lib/features/shop/screens/product_details/widgets/product_meta_data.dart
+++ lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mystore/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:mystore/common/widgets/images/circular_image.dart';
+import 'package:mystore/common/widgets/texts/brand_title_text_with_verified_icon.dart';
 import 'package:mystore/common/widgets/texts/product_price_text.dart';
+import 'package:mystore/common/widgets/texts/product_title_text.dart';
 import 'package:mystore/utils/constants/colors.dart';
+import 'package:mystore/utils/constants/enums.dart';
+import 'package:mystore/utils/constants/image_strings.dart';
 import 'package:mystore/utils/constants/sizes.dart';
 import 'package:mystore/utils/helpers/helper_functions.dart';
 
@@ -47,6 +52,36 @@ class ProductMetaData extends StatelessWidget {
           ],
         ),
         const SizedBox(height: MySizes.spaceBtwItems / 1.5),
+
+        /// Title
+        const MyProductTitleText(title: 'Futuristic Sneakers'),
+        const SizedBox(height: MySizes.spaceBtwItems / 1.5),
+
+        /// Stock Status
+        Row(
+          children: [
+            const MyProductTitleText(title: 'Status'),
+            const SizedBox(width: MySizes.spaceBtwItems),
+            Text('In Stock', style: Theme.of(context).textTheme.titleMedium),
+          ],
+        ),
+        const SizedBox(height: MySizes.spaceBtwItems / 1.5),
+
+        /// Brand
+        Row(
+          children: [
+            MyCircularImage(
+              image: MyImages.shoeIcon,
+              width: 32,
+              height: 32,
+              overlayColor: dark ? MyColors.white : MyColors.black,
+            ),
+            const MyBrandTitleWithVerifiedIcon(
+              title: 'Close up',
+              brandTextSize: TextSizes.medium,
+            )
+          ],
+        ),
       ],
     );
   }
