e8d189d4acc2aa4c14cbe27f5729810bda32ca0a
ima0112 <imandracardenas0@gmail.com>
2024-08-21 17:45:05 -0400

    Subcategory Screen Route

---
+++ lib/features/shop/screens/sub_category/sub_categories.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mystore/common/widgets/appbar/appbar.dart';
+import 'package:mystore/common/widgets/images/rounded_image.dart';
+import 'package:mystore/utils/constants/image_strings.dart';
+import 'package:mystore/utils/constants/sizes.dart';
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MyAppBar(title: Text('Sports sneakers'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(MySizes.defaultSpace),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
