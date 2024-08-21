acb5931e933ef60cb6ec34b874f88ac95cd8eb88
ima0112 <imandracardenas0@gmail.com>
2024-08-21 18:43:29 -0400

    All Products Dropdown

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:mystore/common/widgets/appbar/appbar.dart';
 import 'package:mystore/utils/constants/sizes.dart';
 
@@ -12,6 +13,23 @@ class AllProducts extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(MySizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Dropdown
+              DropdownButtonFormField(
+                decoration:
+                    const InputDecoration(prefixIcon: Icon(Iconsax.sort)),
+                items: ['Name', 'Higher Price', 'Lower Price', 'Newest']
+                    .map((option) => DropdownMenuItem(
+                          value: option,
+                          child: Text(option),
+                        ))
+                    .toList(),
+                onChanged: (value) {},
+              ),
+              const SizedBox(height: MySizes.spaceBtwSections)
+            ],
+          ),
         ),
       ),
     );
