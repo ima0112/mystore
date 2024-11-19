f2d27f1da7f23308dda74c2b570fd1d8f8c25411
ima0112 <imandracardenas0@gmail.com>
2024-11-19 17:04:12 -0500

    Check user status

---
+++ lib/features/authentication/domain/usecase/check_user_status_use_case.dart
@@ -0,0 +1,28 @@
+import 'package:injectable/injectable.dart';
+import 'package:mystore/core/error/failures.dart';
+import 'package:mystore/core/usecases/usecase.dart';
+import 'package:mystore/features/authentication/domain/repositories/authentication_repository.dart';
+
+enum UserStatus { notAuthenticated, emailNotVerified, emailVerified }
+
+@injectable
+class CheckUserStatusUseCase implements UseCase<UserStatus, NoParams> {
+  final AuthenticationRepository repository;
+
+  CheckUserStatusUseCase({required this.repository});
+
+  @override
+  Future<(Failure?, UserStatus?)> call(NoParams params) async {
+    final user = await repository.getCurrentUser();
+
+    if (user.$1 != null) {
+      return (user.$1, null);
+    } else if (user.$2 == null) {
+      return (null, UserStatus.notAuthenticated);
+    } else if (user.$2!.emailVerified) {
+      return (null, UserStatus.emailVerified);
+    } else {
+      return (null, UserStatus.emailNotVerified);
+    }
+  }
+}
