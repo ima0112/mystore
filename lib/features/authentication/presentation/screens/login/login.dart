dc01143b9e25cd58173f17cc630fffbdb1213294
ima0112 <imandracardenas0@gmail.com>
2024-12-09 19:48:21 -0500

    Login process

--- lib/features/authentication/presentation/screens/login/login.dart
+++ lib/features/authentication/presentation/screens/login/login.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mystore/core/common_widgets/styles/spacing_styles.dart';
+import 'package:mystore/core/common_widgets/widgets/loaders/full_screen_loader.dart';
 import 'package:mystore/core/common_widgets/widgets/login_signup/form_divider.dart';
 import 'package:mystore/core/common_widgets/widgets/login_signup/social_buttons.dart';
+import 'package:mystore/core/common_widgets/widgets/snackbar/snackbar.dart';
+import 'package:mystore/core/constants/image_strings.dart';
+import 'package:mystore/core/injection/injection_container.dart';
+import 'package:mystore/core/routing/go_routes.dart';
+import 'package:mystore/features/authentication/presentation/bloc/authentication/authentication_bloc.dart';
+import 'package:mystore/features/authentication/presentation/bloc/sign_in_form/sign_in_form_bloc.dart';
 
 import 'package:mystore/features/authentication/presentation/screens/login/widgets/login_form.dart';
 import 'package:mystore/features/authentication/presentation/screens/login/widgets/login_header.dart';
@@ -14,27 +23,60 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: MySpacingStyles.paddingWithAppBarHeight,
-          child: Column(
-            children: [
-              /// Header: Logo, Title & Subtitle
-              const LoginHeader(),
-
-              /// Form
-              const LoginForm(),
-
-              /// Divider
-              FormDivider(dividerText: MyTexts.orSignInWith.capitalize),
-              const SizedBox(height: MySizes.spaceBtwSections),
-
-              // Footer
-              const SocialButtons(),
-            ],
+    return BlocProvider(
+      create: (context) =>
+          getIt<SignInFormBloc>()..add(const SignInFormEvent.started()),
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthenticationBloc, AuthenticationState>(
+            listener: (context, state) {
+              state.maybeWhen(
+                orElse: () {},
+                error: (message) {
+                  MyFullScreenLoader.stopLoadingDialog(context);
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(MySnackBar.errorSnackBar(message: message));
+                },
+                loggedIn: () {
+                  context.goNamed(MyRoutes.home.name);
+                },
+                loading: () {
+                  MyFullScreenLoader.showLoadingDialog(
+                    context: context,
+                    message: 'Logging you in...',
+                    animation: MyImages.docerAmination,
+                  );
+                },
+              );
+            },
           ),
-        ),
+        ],
+        child: Builder(builder: (context) {
+          return Scaffold(
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: MySpacingStyles.paddingWithAppBarHeight,
+                child: Column(
+                  children: [
+                    /// Header: Logo, Title & Subtitle
+                    const LoginHeader(),
+
+                    /// Form
+                    const LoginForm(),
+
+                    /// Divider
+                    FormDivider(dividerText: MyTexts.orSignInWith.capitalize),
+                    const SizedBox(height: MySizes.spaceBtwSections),
+
+                    // Footer
+                    const SocialButtons(),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
--- lib/features/authentication/presentation/screens/login/widgets/login_form.dart
+++ lib/features/authentication/presentation/screens/login/widgets/login_form.dart
@@ -1,95 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:go_router/go_router.dart';
 import 'package:iconsax/iconsax.dart';
 
 import 'package:mystore/core/constants/sizes.dart';
 import 'package:mystore/core/constants/text_strings.dart';
+import 'package:mystore/core/injection/injection_container.dart';
+import 'package:mystore/core/inputs/user_inputs.dart';
 import 'package:mystore/core/routing/go_routes.dart';
+import 'package:mystore/features/authentication/presentation/bloc/authentication/authentication_bloc.dart';
+import 'package:mystore/features/authentication/presentation/bloc/sign_in_form/sign_in_form_bloc.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     super.key,
   });
 
   @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool obscureText = true;
+  bool rememberMe = false;
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: MySizes.spaceBtwSections,
-        ),
-        child: Column(
-          children: [
-            /// Email
-            TextFormField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.direct_right),
-                labelText: MyTexts.email,
-              ),
-            ),
-            const SizedBox(height: MySizes.spaceBtwInputFields),
+    return BlocListener<SignInFormBloc, SignInFormState>(
+      listener: (context, state) {
+        _emailController.text = state.email.value;
+        _passwordController.text = state.password.value;
 
-            /// Password
-            TextFormField(
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Iconsax.password_check),
-                labelText: MyTexts.password,
-                suffixIcon: Icon(Iconsax.eye_slash),
-              ),
-            ),
-            const SizedBox(height: MySizes.spaceBtwInputFields / 2),
-
-            /// Remember Me & Forget Password
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                /// Remember Me
-                Row(
-                  children: [
-                    Checkbox(value: true, onChanged: (value) {}),
-                    const Text(MyTexts.rememberMe),
-                  ],
+        if (state.isFormValid == true) {
+          context.read<AuthenticationBloc>().add(
+                AuthenticationEvent.signinWithEmailAndPassword(
+                  email: state.email.value.trim(),
+                  password: state.password.value.trim(),
+                  rememberMe: rememberMe,
                 ),
-
-                /// Forget Password
-                TextButton(
-                  onPressed: () {
-                    context.goNamed(MyRoutes.forgetPassword.name);
-                  },
-                  child: const Text(
-                    MyTexts.forgetPassword,
+              );
+        }
+      },
+      child: BlocBuilder<SignInFormBloc, SignInFormState>(
+        builder: (context, state) {
+          return Form(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: MySizes.spaceBtwSections,
+              ),
+              child: Column(
+                children: [
+                  /// Email
+                  TextFormField(
+                    controller: _emailController,
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Iconsax.direct_right),
+                      labelText: MyTexts.email,
+                      errorText: state.email.isPure
+                          ? null
+                          : state.email.error?.message,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: MySizes.spaceBtwSections),
+                  const SizedBox(height: MySizes.spaceBtwInputFields),
 
-            /// Sign In Button
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  context.goNamed('home');
-                },
-                child: const Text(MyTexts.signIn),
+                  /// Password
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: obscureText,
+                    decoration: InputDecoration(
+                      labelText: MyTexts.password,
+                      prefixIcon: const Icon(Iconsax.password_check),
+                      errorText: state.password.isPure
+                          ? null
+                          : state.password.error?.message,
+                      suffixIcon: IconButton(
+                        icon: obscureText
+                            ? const Icon(Iconsax.eye)
+                            : const Icon(Iconsax.eye_slash),
+                        onPressed: () {
+                          setState(() {
+                            obscureText = !obscureText;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwInputFields / 2),
+
+                  /// Remember Me & Forget Password
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// Remember Me
+                      Row(
+                        children: [
+                          Checkbox(
+                              value: rememberMe,
+                              onChanged: (value) {
+                                setState(() {
+                                  rememberMe = value!;
+                                });
+                              }),
+                          const Text(MyTexts.rememberMe),
+                        ],
+                      ),
+
+                      /// Forget Password
+                      TextButton(
+                        onPressed: () {
+                          context.goNamed(MyRoutes.forgetPassword.name);
+                        },
+                        child: const Text(
+                          MyTexts.forgetPassword,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwSections),
+
+                  /// Sign In Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        context.read<SignInFormBloc>().add(
+                              SignInFormEvent.formSubmitted(
+                                email: _emailController.text.trim(),
+                                password: _passwordController.text.trim(),
+                              ),
+                            );
+                      },
+                      child: const Text(MyTexts.signIn),
+                    ),
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwItems),
+
+                  /// Create Account Button
+                  SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {
+                        context.goNamed(MyRoutes.signup.name);
+                      },
+                      child: const Text(MyTexts.createAccount),
+                    ),
+                  ),
+                  const SizedBox(height: MySizes.spaceBtwSections),
+                ],
               ),
             ),
-            const SizedBox(height: MySizes.spaceBtwItems),
-
-            /// Create Account Button
-            SizedBox(
-              width: double.infinity,
-              child: OutlinedButton(
-                onPressed: () {
-                  context.goNamed(MyRoutes.signup.name);
-                },
-                child: const Text(MyTexts.createAccount),
-              ),
-            ),
-            const SizedBox(height: MySizes.spaceBtwSections),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
