cc1ca57b15263651b8fcf87bc1273842c04f0dfd
ima0112 <imandracardenas0@gmail.com>
2024-12-09 19:41:42 -0500

    Remember Me Use Case

---
+++ lib/features/authentication/domain/usecase/remember_me_use_case.dart
@@ -0,0 +1,33 @@
+import 'package:injectable/injectable.dart';
+import 'package:mystore/core/error/failures.dart';
+import 'package:mystore/core/usecases/usecase.dart';
+import 'package:mystore/features/authentication/domain/entities/user_entity.dart';
+import 'package:mystore/features/authentication/domain/repositories/authentication_repository.dart';
+
+@injectable
+class RememberMeUseCase implements UseCase<void, RmemeberMeParams> {
+  final AuthenticationRepository repository;
+
+  RememberMeUseCase({required this.repository});
+
+  @override
+  Future<(Failure?, void)> call(RmemeberMeParams params) async {
+    return await repository.saveCredentials(
+        email: params.email, password: params.password);
+  }
+
+  Future<UserAuthCredentials?> getSavedCredentials() async {
+    return await repository.getCredentials();
+  }
+
+  Future<void> clearSavedCredentials() async {
+    await repository.clearCredentials();
+  }
+}
+
+class RmemeberMeParams {
+  final String email;
+  final String password;
+
+  RmemeberMeParams({required this.email, required this.password});
+}
