14120e9f19a9b1badbcab5ede0dbef8dac1ffce0
ima0112 <imandracardenas0@gmail.com>
2024-12-11 17:57:05 -0500

    Forget password use case

---
+++ lib/features/authentication/domain/usecase/forget_password_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:injectable/injectable.dart';
+import 'package:mystore/core/error/failures.dart';
+import 'package:mystore/core/usecases/usecase.dart';
+import 'package:mystore/features/authentication/domain/repositories/authentication_repository.dart';
+
+@injectable
+class ForgetPasswordUseCase implements UseCase<void, String> {
+  final AuthenticationRepository repository;
+
+  ForgetPasswordUseCase(this.repository);
+
+  @override
+  Future<(Failure?, void)> call(String email) async {
+    return await repository.sendPasswordResetEmail(email);
+  }
+}
