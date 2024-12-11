f454453be8528e8fd899f6261f7221af70c15df8
ima0112 <imandracardenas0@gmail.com>
2024-12-11 17:56:35 -0500

    Forget password method

--- lib/features/authentication/data/repositories/authentication_repository_impl.dart
+++ lib/features/authentication/data/repositories/authentication_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mystore/core/error/exceptions.dart';
 import 'package:mystore/core/error/failures.dart';
+import 'package:mystore/core/local_storage/secure_storage/secure_storage_data_source.dart';
 import 'package:mystore/core/local_storage/shared_preferences/shared_preferences_data_source.dart';
 import 'package:mystore/features/authentication/data/data_sources/local_data_source.dart';
 import 'package:mystore/features/authentication/data/data_sources/remote_data_source.dart';
@@ -12,17 +13,19 @@ import 'package:mystore/features/authentication/domain/repositories/authenticati
 @Injectable(as: AuthenticationRepository)
 class AuthenticationRepositoryImpl implements AuthenticationRepository {
   final SharedPreferencesDataSource sharedPreferencesDataSource;
+  final SecureStorageDataSource secureStorageDataSource;
   final RemoteDataSource remoteDataSource;
   final LocalDataSource localDataSource;
 
   static const rememberEmail = 'remember_email';
   static const rememberPassword = 'remember_password';
 
-  AuthenticationRepositoryImpl({
-    required this.remoteDataSource,
-    required this.localDataSource,
-    required this.sharedPreferencesDataSource,
-  });
+  AuthenticationRepositoryImpl(
+    this.secureStorageDataSource,
+    this.remoteDataSource,
+    this.localDataSource,
+    this.sharedPreferencesDataSource,
+  );
 
   @override
   Future<(Failure?, UserEntity?)> signUpWithEmailAndPassword({
@@ -116,8 +119,7 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   @override
   Future<UserAuthCredentials?> getCredentials() async {
     final email = await sharedPreferencesDataSource.getString(rememberEmail);
-    final password =
-        await sharedPreferencesDataSource.getString(rememberPassword);
+    final password = await secureStorageDataSource.read(rememberPassword);
 
     if (email.isNotEmpty || password.isNotEmpty) {
       return UserAuthCredentials(email: email, password: password);
@@ -129,7 +131,7 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
   @override
   Future<void> clearCredentials() async {
     await sharedPreferencesDataSource.remove(rememberEmail);
-    await sharedPreferencesDataSource.remove(rememberPassword);
+    await secureStorageDataSource.delete(rememberPassword);
   }
 
   @override
@@ -137,7 +139,7 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
       {required String email, required String password}) async {
     try {
       await sharedPreferencesDataSource.saveString(rememberEmail, email);
-      await sharedPreferencesDataSource.saveString(rememberPassword, password);
+      await secureStorageDataSource.write(rememberPassword, password);
       return (null, null);
     } catch (e) {
       return (CacheFailure(e.toString()), null);
@@ -158,6 +160,16 @@ class AuthenticationRepositoryImpl implements AuthenticationRepository {
       return (CacheFailure(e.message), null);
     } catch (e) {
       return (ServerFailure(e.toString()), null);
+    }
+  }
+
+  @override
+  Future<(Failure?, void)> sendPasswordResetEmail(String email) async {
+    try {
+      await remoteDataSource.sendPasswordResetEmail(email);
+      return (null, null);
+    } on ServerException catch (e) {
+      return (ServerFailure(e.message), null);
     }
   }
 }
