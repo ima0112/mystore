b71ca34d8923fb13aa19fdd8c26dad9fb638048c
ima0112 <imandracardenas0@gmail.com>
2024-12-25 14:57:51 -0500

    User Repository and data source

--- lib/common/data/data_source/user/user_local_data_source.dart
+++ lib/common/data/data_source/user/user_local_data_source.dart
@@ -5,6 +5,7 @@ import 'package:mystore/common/data/models/user_model.dart';
 
 abstract class UserLocalDataSource {
   Future<void> cacheUser(IsarUserModel user);
+  Future<UserModel?> getUser();
 }
 
 @Injectable(as: UserLocalDataSource)
@@ -19,6 +20,16 @@ class UserLocalDataSourceImpl implements UserLocalDataSource {
       await _isar.writeTxn(() async {
         await _isar.isarUserModels.put(user);
       });
+    } catch (e) {
+      throw CacheException(e.toString());
+    }
+  }
+
+  @override
+  Future<UserModel?> getUser() async {
+    try {
+      final user = await _isar.isarUserModels.where().findFirst();
+      return user?.toUserModel();
     } catch (e) {
       throw CacheException(e.toString());
     }
--- lib/core/injection/injection_container.config.dart
+++ lib/core/injection/injection_container.config.dart
@@ -18,30 +18,31 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:google_sign_in/google_sign_in.dart' as _i8;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:isar/isar.dart' as _i9;
-import 'package:shared_preferences/shared_preferences.dart' as _i17;
+import 'package:shared_preferences/shared_preferences.dart' as _i16;
 
 import '../../common/data/data_source/secure_storage/secure_storage_data_source.dart'
-    as _i15;
+    as _i14;
 import '../../common/data/data_source/secure_storage/secure_storage_data_source_impl.dart'
-    as _i16;
+    as _i15;
 import '../../common/data/data_source/shared_preferences/shared_preferences_data_source.dart'
-    as _i18;
+    as _i17;
 import '../../common/data/data_source/shared_preferences/shared_preferences_data_source_impl.dart'
-    as _i19;
-import '../../common/data/data_source/user_local_data_source.dart' as _i21;
-import '../../common/data/data_source/user_remote_data_source.dart' as _i22;
+    as _i18;
+import '../../common/data/data_source/user/user_local_data_source.dart' as _i20;
+import '../../common/data/data_source/user/user_remote_data_source.dart'
+    as _i21;
 import '../../common/data/repositories/network_info_repository_impl.dart'
     as _i13;
 import '../../common/data/repositories/onboarding_repository_impl.dart' as _i35;
-import '../../common/data/repositories/user_repository_impl.dart' as _i24;
+import '../../common/data/repositories/user_repository_impl.dart' as _i23;
 import '../../common/domain/repositories/network_info_repository.dart' as _i12;
 import '../../common/domain/repositories/onboarding_repository.dart' as _i34;
-import '../../common/domain/repositories/user_repository.dart' as _i23;
+import '../../common/domain/repositories/user_repository.dart' as _i22;
 import '../../common/domain/usecases/form_validation_use_case.dart' as _i7;
 import '../../common/domain/usecases/is_connected_use_case.dart' as _i30;
 import '../../common/domain/usecases/onboarding_use_case.dart' as _i36;
 import '../../features/authentication/data/data_sources/auth_remote_data_source.dart'
-    as _i14;
+    as _i24;
 import '../../features/authentication/data/repositories/authentication_repository_impl.dart'
     as _i26;
 import '../../features/authentication/domain/repositories/authentication_repository.dart'
@@ -66,7 +67,7 @@ import '../../features/authentication/presentation/bloc/onboarding_controller/on
 import '../../features/authentication/presentation/bloc/sign_in_form/sign_in_form_bloc.dart'
     as _i41;
 import '../../features/authentication/presentation/bloc/sign_up_form/sign_up_form_bloc.dart'
-    as _i20;
+    as _i19;
 import '../utils/http_client/dio_client.dart' as _i33;
 import '../utils/http_client/http_client.dart' as _i32;
 import '../utils/local_storage/isar_storage.dart' as _i11;
@@ -102,42 +103,45 @@ Future<_i1.GetIt> $initGetIt(
   );
   gh.factory<_i10.MyLocalStorage>(() => _i11.MyIsarStorage(gh<_i9.Isar>()));
   gh.lazySingleton<_i12.NetworkInfoRepository>(() => _i13.NetworkInfoImpl());
-  gh.factory<_i14.RemoteDataSource>(() => _i14.RemoteDataSourceImpl(
-        gh<_i4.FirebaseAuth>(),
-        gh<_i5.FirebaseFirestore>(),
-        gh<_i8.GoogleSignIn>(),
-      ));
-  gh.lazySingleton<_i15.SecureStorageDataSource>(
-      () => _i16.SecureStorageDataSourceImpl(gh<_i6.FlutterSecureStorage>()));
-  await gh.factoryAsync<_i17.SharedPreferences>(
+  gh.lazySingleton<_i14.SecureStorageDataSource>(
+      () => _i15.SecureStorageDataSourceImpl(gh<_i6.FlutterSecureStorage>()));
+  await gh.factoryAsync<_i16.SharedPreferences>(
     () => registerModule.prefs(),
     preResolve: true,
   );
-  gh.lazySingleton<_i18.SharedPreferencesDataSource>(
-      () => _i19.SharedPreferencesDataSourceImpl(gh<_i17.SharedPreferences>()));
-  gh.factory<_i20.SignUpFormBloc>(
-      () => _i20.SignUpFormBloc(gh<_i7.FormValidationUseCase>()));
+  gh.lazySingleton<_i17.SharedPreferencesDataSource>(
+      () => _i18.SharedPreferencesDataSourceImpl(gh<_i16.SharedPreferences>()));
+  gh.factory<_i19.SignUpFormBloc>(
+      () => _i19.SignUpFormBloc(gh<_i7.FormValidationUseCase>()));
   gh.factory<String>(
     () => registerModule.baseUrl,
     instanceName: 'BaseUrl',
   );
-  gh.factory<_i21.UserLocalDataSource>(
-      () => _i21.UserLocalDataSourceImpl(gh<_i9.Isar>()));
-  gh.factory<_i22.UserRemoteDataSource>(
-      () => _i22.UserRemoteDataSourceImpl(gh<_i5.FirebaseFirestore>()));
-  gh.factory<_i23.UserRepository>(() => _i24.UserRepositoryImpl(
-        gh<_i21.UserLocalDataSource>(),
-        gh<_i22.UserRemoteDataSource>(),
+  gh.factory<_i20.UserLocalDataSource>(
+      () => _i20.UserLocalDataSourceImpl(gh<_i9.Isar>()));
+  gh.factory<_i21.UserRemoteDataSource>(() => _i21.UserRemoteDataSourceImpl(
+        gh<_i5.FirebaseFirestore>(),
+        gh<_i4.FirebaseAuth>(),
+      ));
+  gh.factory<_i22.UserRepository>(() => _i23.UserRepositoryImpl(
+        gh<_i20.UserLocalDataSource>(),
+        gh<_i21.UserRemoteDataSource>(),
+      ));
+  gh.factory<_i24.AuthRemoteDataSource>(() => _i24.AuthRemoteDataSourceImpl(
+        gh<_i4.FirebaseAuth>(),
+        gh<_i5.FirebaseFirestore>(),
+        gh<_i8.GoogleSignIn>(),
       ));
   gh.factory<_i25.AuthenticationRepository>(
       () => _i26.AuthenticationRepositoryImpl(
-            gh<_i15.SecureStorageDataSource>(),
-            gh<_i14.RemoteDataSource>(),
-            gh<_i21.UserLocalDataSource>(),
-            gh<_i18.SharedPreferencesDataSource>(),
+            gh<_i14.SecureStorageDataSource>(),
+            gh<_i24.AuthRemoteDataSource>(),
+            gh<_i20.UserLocalDataSource>(),
+            gh<_i17.SharedPreferencesDataSource>(),
+            gh<_i21.UserRemoteDataSource>(),
           ));
-  gh.factory<_i27.CheckUserStatusUseCase>(() => _i27.CheckUserStatusUseCase(
-      repository: gh<_i25.AuthenticationRepository>()));
+  gh.factory<_i27.CheckUserStatusUseCase>(
+      () => _i27.CheckUserStatusUseCase(repository: gh<_i22.UserRepository>()));
   gh.lazySingleton<_i28.Dio>(
       () => registerModule.client(gh<String>(instanceName: 'BaseUrl')));
   gh.factory<_i29.ForgetPasswordUseCase>(
@@ -148,7 +152,7 @@ Future<_i1.GetIt> $initGetIt(
       _i31.LogoutUserUseCase(repository: gh<_i25.AuthenticationRepository>()));
   gh.factory<_i32.MyHttpClient>(() => _i33.MyDioClient(dio: gh<_i28.Dio>()));
   gh.lazySingleton<_i34.OnboardingRepository>(() =>
-      _i35.OnboardingRepositoryImpl(gh<_i18.SharedPreferencesDataSource>()));
+      _i35.OnboardingRepositoryImpl(gh<_i17.SharedPreferencesDataSource>()));
   gh.lazySingleton<_i36.OnboardingUseCase>(() =>
       _i36.OnboardingUseCase(repository: gh<_i34.OnboardingRepository>()));
   gh.lazySingleton<_i37.RegisterUserUseCase>(
